f7322bd56902fa99f1a5bf31fc910d729845ce28
Ray Toal <rtoal@lmu.edu>
2017-01-20 00:21:18 -0800

    Make the linter happy

--- server/gameutil.js
+++ server/gameutil.js
@@ -1,20 +1,16 @@
-exports.clamp = (value, low, high) => {
-  return Math.max(low, Math.min(high, value));
-}
+exports.clamp = (value, low, high) => Math.max(low, Math.min(high, value));
 
-exports.randomPoint = (maxX, maxY) => {
-  return [
-    Math.floor(Math.random() * maxX),
-    Math.floor(Math.random() * maxY),
-  ];
-}
+exports.randomPoint = (maxX, maxY) => [
+  Math.floor(Math.random() * maxX),
+  Math.floor(Math.random() * maxY),
+];
 
 // Returns an array of size n with the values 0..n-1 in random positions.
-exports.permutationArray = (n) => {
+exports.permutation = (n) => {
   const array = Array(n).fill(0).map((_, i) => i);
   for (let i = n - 1; i > 0; i -= 1) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
-}
+};
